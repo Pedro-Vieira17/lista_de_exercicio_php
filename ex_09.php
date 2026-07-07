02260b10e57c204f3227b16e62f7421496603e61
Pedro <pedro_vieira-juni@estudante.sesisenai.org.br>
2026-07-07 08:45:23 -0300

    add

---
+++ ex_09.php
@@ -0,0 +1,18 @@
+<?php
+
+    function analisarNumero($numero){
+
+    if($numero % 2 == 0){
+       $paridade = "Par";
+    }else{
+        $paridade = "Ímpar"
+    }
+
+    
+
+   
+
+    }
+
+
+?>
